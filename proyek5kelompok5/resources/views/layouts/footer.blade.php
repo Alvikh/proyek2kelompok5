ecd033855238372a3ddeb789d74f4dab4fc57ece
Alvikh <khoirunnisaalvi4@gmail.com>
2024-01-01 19:14:38 +0700

    menambahkan API whatsapp

--- proyek5kelompok5/resources/views/layouts/footer.blade.php
+++ proyek5kelompok5/resources/views/layouts/footer.blade.php
@@ -22,7 +22,7 @@
     <span class="u-file-icon u-icon u-icon-3" data-href="https://twitter.com/SusantoMulyono2"><img src="{{ asset ('img/x.png') }}" alt=""></span>
     <span class="u-file-icon u-icon u-icon-4" data-href="mailto:susanto.developer@yotoagrodita.com"><img src="{{ asset ('img/gmail.png') }}" alt=""></span>
     <a href="{{ url('hubungikami') }}" class="u-active-none u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-base u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-active-custom-color-3 u-text-hover-black u-top-left-radius-0 u-top-right-radius-0 u-btn-3">Hubungi Kami</a>
-    <a href="#" class="u-active-none u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-base u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-active-custom-color-3 u-text-hover-black u-top-left-radius-0 u-top-right-radius-0 u-btn-4">Chat Langsung</a>
+    <a href="https://api.whatsapp.com/send?phone=6289522399121&text=Permisi,%20Saya%20ingin%20bertanya" class="u-active-none u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-base u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-active-custom-color-3 u-text-hover-black u-top-left-radius-0 u-top-right-radius-0 u-btn-4">Chat Langsung</a>
     <p class="u-custom-font u-font-montserrat u-text u-text-default u-text-2"> Copyright © 2023 Kelompok 5</p>
   </div>
 </footer>
